329a5481f65a2654ab52de76660edbdd5137eb67
Cielo Vizcaino <amarilisvizcaino88@gmail.com>
2026-09-17 12:48:42 -0500

    Corregir sitio para GitHub Pages

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById("anio").textContent = new Date().getFullYear();
 
-var boton = document.getElementById("toggleButton");
+var boton = document.getElementById("toggleHabilidades");
 var lista = document.getElementById("listaHabilidades");
 
 console.log("Habilidades listadas:", lista.children.length);
 
-boton.addEventListerner("click", function(){
+boton.addEventListener("click", function(){
     lista.classList.toggle("oculto");
+    boton.setAttribute("aria-expanded", String(!lista.classList.contains("oculto")));
 });
